4d17c636d4cb993294b2446c77179846b6a3218f
ramdhanll <erdevlop@gmail.com>
2021-02-19 19:06:15 +0700

    add item to cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react'
-// import Navbar from './components/Navbar/Navbar'
-// import Products from './components/Products/Products'
+import React, { useEffect, useState } from 'react'
+import { commerce } from './lib/commerce'
 
-import { Navbar, Products } from './components'
+import { Navbar, Products, Cart } from './components'
 
 const App = () => {
+   const [products, setProducts] = useState([])
+   const [cart, setCart] = useState({})
+
+   const fetchProducts = async () => {
+      const {data} = await commerce.products.list()
+      setProducts(data)
+   }
+
+   const fetchCard = async () => {
+      setCart(await commerce.cart.retrieve())
+   }
+
+   const handleAddToCart = async (productId, quantity) => {
+      const item = await commerce.cart.add(productId, quantity)
+
+      setCart(item.cart)
+   }
+
+   useEffect(() => {
+      fetchProducts()
+      fetchCard()
+   }, [])
+
+   console.log(cart)
    return (
       <div>
-         <Navbar />
-         <Products />
+         <Navbar totalItems={cart.total_items} />
+         {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+         <Cart cart={cart} />
       </div>
    )
 }
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -4,27 +4,30 @@ import { AddShoppingCart } from '@material-ui/icons'
 
 import useStyles from './styles'
 
-const Product = ({product}) => {
+const Product = ({product, onAddToCart}) => {
    const classes = useStyles()
-   
+
    return (
-      <Card className={classes.root} >
-         <CardMedia className={classes.media} image={product.image} title={product.name} />
+         <Card className={classes.root}  >
+         <CardMedia className={classes.media} image={product.media.source} title={product.name} />
          <CardContent>
             <div className={classes.cardContent}>
                <Typography variant="h5" gutterBottom>
                   {product.name}
                </Typography>
                <Typography variant="h5">
-                  {product.price}
+                  {product.price.formatted_with_symbol}
                </Typography>
             </div>
-            <Typography variant="body2" color="textSecondary">
-               {product.description}
-            </Typography>
+            <Typography 
+               variant="body2" 
+               color="textSecondary" 
+               dangerouslySetInnerHTML={{ 
+                  __html: product.description.length > 100 ? 
+                     `${product.description.substring(0, 100)}...` : product.description}} />
          </CardContent>
          <CardActions>
-            <IconButton aria-label="Add to Cart">
+            <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
                <AddShoppingCart/>
             </IconButton>
          </CardActions>
